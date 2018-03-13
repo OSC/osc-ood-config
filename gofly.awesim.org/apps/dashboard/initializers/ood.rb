98d86b4671864809f3edf731657aab165896bb5e
Eric Franz <efranz@osc.edu>
2018-03-13 18:40:26 -0400

    remove old gofly config

--- gofly.awesim.org/apps/dashboard/initializers/ood.rb
+++
@@ -1,6 +0,0 @@
-# hide all menu options
-NavConfig.categories = []
-
-# enable dev mode for all configured app developers
-Configuration.app_development_enabled = UsrRouter.base_path.directory?
-Configuration.app_sharing_facls_enabled = true

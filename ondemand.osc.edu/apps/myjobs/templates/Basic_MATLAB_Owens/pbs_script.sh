9c46f467adc47c2c40c0402b26305cc66439ffda
SI\azhu <azhu@osc.edu>
2018-08-06 09:35:35 -0400

    copy templates from awesim to ondemand

--- ondemand.osc.edu/apps/myjobs/templates/Basic_MATLAB_Owens/pbs_script.sh
+++ ondemand.osc.edu/apps/myjobs/templates/Basic_MATLAB_Owens/pbs_script.sh
@@ -1,11 +1,21 @@
-#PBS -N disable_multithreading
+#PBS -N matlab_example
 #PBS -l walltime=00:10:00
 #PBS -l nodes=1:ppn=28
 #PBS -j oe
 
-#  A Basic Parallel MATLAB Job for the OSC Owens Cluster
+
+#  A Basic MATLAB Job for the OSC Owens Cluster
 #  https://www.osc.edu/resources/available_software/software_list/matlab
 
+
+# load MATLAB module
 module load matlab
-matlab -singleCompThread -nodisplay -nodesktop < hello.m
+
+# transfer MATLAB script file to TMPDIR
+cd $PBS_O_WORKDIR
+cp hello.m $TMPDIR
+cd $TMPDIR
+
+# run MATLAB script
+matlab -nodisplay -nodesktop < hello.m
 # end of example file

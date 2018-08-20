88d97a53e254e2e8363a7f9bb01363f21dfe352a
AriettaZ <azhu@osc.edu>
2018-08-20 14:36:12 -0400

    add templates

---
+++ apps.awesim.org/apps/myjobs/templates/Basic_Schrodinger_Serial_Job_Owens/pbs_script.sh
@@ -0,0 +1,33 @@
+#PBS -N ondemand/sys/myjobs/basic_schrodinger_serial
+#PBS -j oe
+#PBS -l walltime=0:10:00
+#PBS -l nodes=1:ppn=28
+#PBS -l software=glide+1
+#PBS -S /bin/sh
+qstat -f $PBS_JOBID
+export
+
+#   A Basic Schrodinger Serial Job for the OSC Owens Cluster
+#   https://www.osc.edu/resources/available_software/software_list/schrodinger
+
+#
+# The following lines set up the Schrodinger environment
+#
+module load schrodinger
+#
+# Move to the directory where the job was submitted
+#
+cd $PBS_O_WORKDIR
+pbsdcp -p /users/appl/srb/workshops/compchem/schrodinger/receptor_glide.in $TMPDIR
+cd $TMPDIR
+host=`cat $PBS_NODEFILE|head -1`
+nproc=`cat $PBS_NODEFILE|wc -l`
+#
+# Run schrodinger
+#
+glide -WAIT -HOST ${host}:${nproc} receptor_glide.in
+ls -l
+#
+# Now, copy data (or move) back once the simulation has completed
+#
+pbsdcp -p '*' $PBS_O_WORKDIR
---
+++ ondemand.osc.edu/apps/myjobs/templates/Basic_R_Parallel_Job_Owens/pbs_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l walltime=2:10:00
+#PBS -l nodes=2:ppn=28
+#PBS -j oe
+#PBS -N ondemand/sys/myjobs/basic_r_parallel
+
+#  A Basic R Parallel Job for the OSC Owens Cluster
+#  https://www.osc.edu/resources/available_software/software_list/r
+
+#
+# The following lines set up the R environment
+#
+ml openmpi/1.10
+ml R/3.3.1
+
+#
+# Move to the directory where the job was submitted
+#
+cd $PBS_O_WORKDIR
+# parallel R: submit job with one MPI master
+mpirun -np 1 R --slave < parallel_testing.R

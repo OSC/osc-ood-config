d360101e5482a497703b7414d2fdce71f7b59681
Eric Franz <efranz@osc.edu>
2018-10-09 17:04:20 -0400

    totalsim update for whitelist mode; newlines @ eof

--- apps.totalsim.us/apps/dashboard/initializers/ood.rb
+++ apps.totalsim.us/apps/dashboard/initializers/ood.rb
@@ -1,5 +1,6 @@
 # hide all menu options
 NavConfig.categories = []
+NavConfig.categories_whitelist=true
 
 # enable dev mode for all configured app developers
 Configuration.app_development_enabled = UsrRouter.base_path.directory?

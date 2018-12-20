c5da7b4b4900665010ccb1a6dbe5801dfd2ca5ee
Eric Franz <efranz@osc.edu>
2018-12-20 11:25:56 -0500

    app development enabled should use env by default

--- ondemand.osc.edu/apps/dashboard/initializers/ood.rb
+++ ondemand.osc.edu/apps/dashboard/initializers/ood.rb
@@ -9,16 +9,3 @@ OodFilesApp.candidate_favorite_paths.tap do |paths|
 end
 
 NavConfig.categories_whitelist=true
-
-# don't show develop dropdown unless you are setup for app development
-#
-# FIXME: this should work if the PUN tells the dashboard what the app schema is
-# Configuration.app_development_enabled = DevRouter.base_path.directory?
-#
-# HACK: workaround for new nginx_config default
-# Several problems
-#   1. We want the real path to be used, that is ~/ondemand/dev when ssh etc.
-#   2. We want the configured path (i.e. local disk) to be used when determining if a user should have access
-#
-#   The short answer is to maintain the status quo
-Configuration.app_development_enabled = File.directory? "/var/www/ood/apps/dev/#{OodSupport::Process.user.name}/gateway"

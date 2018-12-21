ee77d3a23220e2228b4b00b5802e1dd7678c6422
Tyler Harris <tharris@osc.edu>
2018-12-21 13:51:28 -0500

    Fix GROMACS and add Pitzer version

--- apps.awesim.org/apps/myjobs/templates/Basic_GROMACS_Parallel_Job_Pitzer/pbs_script.sh
+++ apps.awesim.org/apps/myjobs/templates/Basic_GROMACS_Parallel_Job_Pitzer/pbs_script.sh
@@ -1,15 +1,15 @@
-# Owens example batch script for GROMACS 2018 using the non MPI, non GPU mdrun;
+# Pitzer example batch script for GROMACS 2018 using the non MPI, non GPU mdrun;
 # thus nodes must be 1 and only OpenMP based parallelism is used.
 # GROMACS Tutorial of Lysozyme step 8 production MD :
 # http://www.bevanlab.biochem.vt.edu/Pages/Personal/justin/gmx-tutorials/lysozyme/index.html
 
-#PBS -N ondemand/sys/myjobs/basic_gromacs_serial
+#PBS -N ondemand/sys/myjobs/basic_gromacs_parallel
 #PBS -j oe
 #PBS -l walltime=2:10:00
-#PBS -l nodes=1:ppn=28
+#PBS -l nodes=1:ppn=40
 #PBS -S /bin/bash
 
-#  A Basic GROMACS Serial Job for the OSC Owens Cluster
+#  A Basic GROMACS Serial Job for the OSC Pitzer Cluster
 #  https://www.osc.edu/resources/available_software/software_list/gromacs
 
 # turn off verbosity for noisy module commands
@@ -18,11 +18,8 @@ set +vx
 #
 # The following lines set up the environment
 #
-module unload mvapich2
-module unload intel
-module load intel/18.0.3
-module load mvapich2/2.3
-module load gromacs/2018.2
+module purge
+module load intel/18.0.4 mvapich2/2.3 gromacs/2018.2
 module list
 set -vx
 

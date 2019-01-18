b97a52bb476f6119b817396ae29c10c3709fe902
AriettaZ <azhu@osc.edu>
2019-01-18 10:52:40 -0500

    add amber for pitzer

--- apps.awesim.org/apps/myjobs/templates/Basic_AMBER_Job_Pitzer/basic_amber.sh
+++ apps.awesim.org/apps/myjobs/templates/Basic_AMBER_Job_Pitzer/basic_amber.sh
@@ -1,7 +1,7 @@
 #PBS -N ondemand/sys/myjobs/basic_amber
 #PBS -j oe
 #PBS -l walltime=0:10:00
-#PBS -l nodes=2:ppn=40
+#PBS -l nodes=2:ppn=12
 #
 # The following lines set up the Amber environment
 # Further Details at
@@ -18,26 +18,14 @@ echo "PBS_O_WORKDIR=$PBS_O_WORKDIR"
 cd $TMPDIR
 #
 # The file names below may need to be changed
-set MDIN=mdin
-set MDOUT=mdout
-set MDINFO=mdinfo
-set PRMTOP=prmtop
-set INPCRD=inpcrd.equil
-set REFC=refc
-set MDCRD=mdcrd
-set MDVEL=mdvel
-set MDEN=mden
-set RESTRT=restrt
-cp -p /users/appl/srb/workshops/compchem/amber/$MDIN .
-cp -p /users/appl/srb/workshops/compchem/amber/$PRMTOP .
-cp -p /users/appl/srb/workshops/compchem/amber/$INPCRD .
-cp -p /users/appl/srb/workshops/compchem/amber/$REFC .
-#cp -p $PBS_O_WORKDIR/$RESTRT .
+cp -p /users/appl/srb/workshops/compchem/amber/mdin .
+cp -p /users/appl/srb/workshops/compchem/amber/prmtop .
+cp -p /users/appl/srb/workshops/compchem/amber/inpcrd.equil .
 #
 # Some jobs may require the -O option on the Amber command lines below
 #mpiexec $AMBERHOME/bin/sander.MPI -i $MDIN -o stdout -inf $MDINFO -p $PRMTOP -c $INPCRD -ref $REFC -x $MDCRD -v $MDVEL -e $MDEN -r $RESTRT
 # Some jobs may use pmemd which consumes less walltime than sander.
-mpiexec $AMBERHOME/bin/pmemd.MPI -i $MDIN -o $MDOUT -inf $MDINFO -p $PRMTOP -c $INPCRD -ref $REFC -x $MDCRD -v $MDVEL -e $MDEN -r $RESTRT
+mpiexec $AMBERHOME/bin/pmemd.MPI -i mdin -o mdout -p prmtop -c inpcrd.equil
 ls -al
-cp -p $MDOUT $MDINFO $MDCRD $MDVEL $MDEN $RESTRT $PBS_O_WORKDIR
-cat $MDOUT
+cp -p mdout $PBS_O_WORKDIR
+cat mdout

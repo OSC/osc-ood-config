79f4ca4106232783506bed768cc4c2d8e7c8f4b4
Morgan Rodgers <mrodgers@osc.edu>
2019-08-20 15:08:35 -0400

    Add project and scratch directories to the file system favs

--- stats2480.osc.edu/apps/dashboard/initializers/stats2480.rb
+++ stats2480.osc.edu/apps/dashboard/initializers/stats2480.rb
@@ -4,6 +4,16 @@ require 'nav_config'
 NavConfig.categories = ["Files", "Clusters"]
 NavConfig.categories_whitelist = true
 
+OodFilesApp.candidate_favorite_paths.tap do |paths|
+  # add project space directories
+  projects = User.new.groups.map(&:name).grep(/^P./)
+  paths.concat projects.map { |p| Pathname.new("/fs/project/#{p}")  }
+
+  # add scratch space directories
+  paths << Pathname.new("/fs/scratch/#{User.new.name}")
+  paths.concat projects.map { |p| Pathname.new("/fs/scratch/#{p}")  }
+end
+
 require 'application_controller'
 class ApplicationController
     def usr_apps

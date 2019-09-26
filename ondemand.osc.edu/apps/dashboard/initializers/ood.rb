bf8583badd33ba86e53fe2091b05d001710ccad3
Morgan Rodgers <mrodgers@osc.edu>
2019-09-26 11:53:09 -0400

    Update the proposed config with the following changes:
    - Lowercase options since we do not need to follow environment variable conventions any more
    - Document what each new variable replaces
    - Removed unecessary `ood_` prefix
    - Add ERB'd user_file_system_locations (note that this will require all configuration being preprocessed by a Ruby tool)
    - Completely removed:
        - APP_TOKEN
        - BC_APP_TOKEN
        - BC_RENDER_FORMAT
        - OOD_DATAROOT
        - OOD_FILE_EDITOR
        - OOD_SHELL
        - OOD_SHELL_URL
    - Moved help menu links into links namespace
        - DASHBOARD_2FA_URL
        - DASHBOARD_DEV_DOCS_URL
        - DASHBOARD_DOCS_URL
        - OOD_DASHBOARD_PASSWD_URL
        - OOD_DASHBOARD_SUPPORT_URL
    - Moved MOTD settings into the motd namespace
    - Removed unneeded candidate_favorite_paths from the custom initializer

--- ondemand.osc.edu/apps/dashboard/initializers/ood.rb
+++ ondemand.osc.edu/apps/dashboard/initializers/ood.rb
@@ -1,11 +1,1 @@
-OodFilesApp.candidate_favorite_paths.tap do |paths|
-  # add project space directories
-  projects = User.new.groups.map(&:name).grep(/^P./)
-  paths.concat projects.map { |p| Pathname.new("/fs/project/#{p}")  }
-
-  # add scratch space directories
-  paths << Pathname.new("/fs/scratch/#{User.new.name}")
-  paths.concat projects.map { |p| Pathname.new("/fs/scratch/#{p}")  }
-end
-
 NavConfig.categories_whitelist=true

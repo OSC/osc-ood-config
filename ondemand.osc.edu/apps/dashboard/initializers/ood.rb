9d15f442209eb2071dcb415483bea58f20157a2c
Trey Dockendorf <tdockendorf@osc.edu>
2019-10-04 08:27:45 -0400

    Set OOD_DASHBOARD_HELP_CUSTOM_URL based on hostname

--- ondemand.osc.edu/apps/dashboard/initializers/ood.rb
+++ ondemand.osc.edu/apps/dashboard/initializers/ood.rb
@@ -9,3 +9,15 @@ OodFilesApp.candidate_favorite_paths.tap do |paths|
 end
 
 NavConfig.categories_whitelist=true
+
+require 'socket'
+hostname = Socket.gethostname
+case hostname
+when /dev/
+  idp = 'https://idp-dev.osc.edu'
+when /test/
+  idp = 'https://idp-test.osc.edu'
+else
+  idp = 'https://idp.osc.edu'
+end
+ENV['OOD_DASHBOARD_HELP_CUSTOM_URL'] = "#{idp}/auth/realms/osc/account/identity"

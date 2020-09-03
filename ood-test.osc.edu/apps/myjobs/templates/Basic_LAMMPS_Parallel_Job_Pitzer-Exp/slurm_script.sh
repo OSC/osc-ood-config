5f31d4abaa871b1c556cdabff0d40bb705aa583c
msquee <msqueo@osc.edu>
2020-09-03 14:28:17 -0400

    Fix LAMMPS for Slurm
    * Set --export=ALL manually

--- ood-test.osc.edu/apps/myjobs/templates/Basic_LAMMPS_Parallel_Job_Pitzer-Exp/slurm_script.sh
+++ ood-test.osc.edu/apps/myjobs/templates/Basic_LAMMPS_Parallel_Job_Pitzer-Exp/slurm_script.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 #SBATCH -J ondemand/sys/myjobs/basic_lammps_parallel
 #SBATCH --time=00:30:00
-#SBATCH --nodes=2 
-#SBATCH --ntasks-per-node=40
-#SBATCH -S /bin/bash
+#SBATCH --nodes=2
 
 #  A Basic LAMMPS Parallel Job for the OSC Pitzer cluster
 # https://www.osc.edu/resources/available_software/software_list/lammps
 
 # emit verbose details on the job's queuing.
 scontrol show job $SLURM_JOBID
+
 #
 # The following lines set up the LAMMPS environment
 #
-module load lammps
-module list
-export OMP_NUM_THREADS=80 # this must match nodes * ppn
+module load intel/19.0.5 mvapich2/2.3.4 lammps/3Mar20
+
 #
 # Move to the directory where the job was submitted
 #
-sbcast -p /users/appl/srb/workshops/compchem/lammps/in.crack $SLURM_SUBMIT_DIR
-cd $SLURM_SUBMIT_DIR
-sbcast -p in.crack $TMPDIR
+
 cd $TMPDIR
+sbcast -p /users/appl/srb/workshops/compchem/lammps/in.crack $TMPDIR/in.crack
+
 #
 # Run LAMMPS
 #
-lammps < in.crack
-#
-# Now, copy data (or move) back once the simulation has completed
-#
-sgather -p '*' $SLURM_SUBMIT_DIR
-ls -al
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+srun --export=ALL -n 2 lammps < $TMPDIR/in.crack

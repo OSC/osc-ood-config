309df1f015dcda91c05fb307b06b3293494b963a
KinanAlAttar <alattar.2@osu.edu>
2020-09-04 10:20:31 -0400

    Revert "revert this: temp rm broken Gaussian template"
    This reverts commit de01f9e40ee74f2da4d17286da59436c27a2bbef.

---
+++ ood-test.osc.edu/apps/myjobs/templates/Basic_Gaussian_Pitzer-Exp/slurm_script.sh
@@ -0,0 +1,29 @@
+#!/bin/csh
+#SBATCH -J ondemand/sys/myjobs/basic_gaussian
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+
+# A Basic Gaussian Job for the OSC Pitzer Cluster
+#   https://www.osc.edu/resources/available_software/software_list/gaussian
+
+# SLURM_SUBMIT_DIR refers to the directory from which the job was submitted.
+
+#
+# The following lines set up the Gaussian environment
+#
+module load gaussian
+#
+# Move to the directory where the job was submitted
+#
+cd $TMPDIR
+cp -p /users/appl/srb/workshops/compchem/gaussian/glucose.com .
+#
+# Run Gaussian
+#
+g16 < glucose.com
+ls -al
+#
+# Now, copy data (or move) back once the simulation has completed
+#
+cp -p glucose.log glucose.chk $SLURM_SUBMIT_DIR

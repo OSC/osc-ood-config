34263b50ebb6e356c0d5edfcf31967c09dcf771f
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-09-29 10:32:58 -0400

    only update myjobs DB records if the database exists

--- ondemand.osc.edu/apps/myjobs/initializers/ood_appkit_pitzer_exp_fix.rb
+++ ondemand.osc.edu/apps/myjobs/initializers/ood_appkit_pitzer_exp_fix.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do |config|
   ActiveSupport.on_load(:active_record) do
     Workflow.where(batch_host: 'pitzer-exp').update_all(batch_host: 'pitzer')
-  end
+  end if File.exist?("#{ENV['HOME']}/ondemand/data/sys/myjobs/production.sqlite3")
 end
 

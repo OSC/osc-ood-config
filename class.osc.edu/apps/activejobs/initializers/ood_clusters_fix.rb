673f511d3e9d006ef946a60c8dd6b8aa847a247b
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-11-05 16:43:04 -0500

    missed ruby in class.osc.edu, but activejobs isn't enabled there, so just remove it entirely

--- class.osc.edu/apps/activejobs/initializers/ood_clusters_fix.rb
+++
@@ -1,7 +0,0 @@
-OodAppkit.clusters = OodCore::Clusters.new(
-  OodAppkit.clusters.reject { |cluster| [:quick_ruby, :quick_pitzer].include?(cluster.id) }
-)
-
-if defined?(OODClusters)
-  OODClusters = OodCore::Clusters.new(OodAppkit.clusters.select(&:job_allow?))
-end

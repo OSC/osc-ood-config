eed46d2c0014736f32cafbc4b745050a92e8bf0c
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-12-15 16:03:03 -0500

    job composer also change owens-slurm records to owens

--- apps.awesim.org/apps/myjobs/initializers/ood_appkit_pitzer_exp_fix.rb
+++ apps.awesim.org/apps/myjobs/initializers/ood_appkit_pitzer_exp_fix.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do |config|
   ActiveSupport.on_load(:active_record) do
     Workflow.where(batch_host: 'pitzer-exp').update_all(batch_host: 'pitzer')
+    Workflow.where(batch_host: 'owens-slurm').update_all(batch_host: 'owens')
   end if Configuration.production_database_path.file?
 end
 

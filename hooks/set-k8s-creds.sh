9b7c8cd9c3d6bcd50c1f876fdf33ce7cb2cda38b
Jeff Ohrstrom <johrstrom@osc.edu>
2021-05-11 17:50:21 -0400

    v2.0.6 expects OOD_ prefixed env variables (#168)

--- hooks/set-k8s-creds.sh
+++ hooks/set-k8s-creds.sh
@@ -17,5 +17,5 @@ sudo -u "$ONDEMAND_USERNAME" kubectl config set-credentials "$K8S_USERNAME" \
    --auth-provider-arg=idp-issuer-url="$TOKEN_URL" \
    --auth-provider-arg=client-id="$CLIENT_ID" \
    --auth-provider-arg=client-secret="$CLIENT_SECRET" \
-   --auth-provider-arg=refresh-token="$OIDC_REFRESH_TOKEN" \
-   --auth-provider-arg=id-token="$OIDC_ACCESS_TOKEN"
+   --auth-provider-arg=refresh-token="$OOD_OIDC_REFRESH_TOKEN" \
+   --auth-provider-arg=id-token="$OOD_OIDC_ACCESS_TOKEN"

8b788949330a4d3319222e9c01ff76bd7fa40e17
Gerald Byrket <gbyrket@osc.edu>
2022-12-20 11:41:44 -0500

    Removed /fs/ess/scratch & /fs/project from Files Shortcuts

--- ondemand.osc.edu/apps/dashboard/initializers/ood.rb
+++ ondemand.osc.edu/apps/dashboard/initializers/ood.rb
@@ -3,14 +3,18 @@ def add_paths
   OodFilesApp.candidate_favorite_paths.tap do |paths|
     # add project space directories
     projects = User.new.groups.map(&:name).grep(/^P./)
-    paths.concat projects.map { |p| Pathname.new("/fs/project/#{p}")  }
+    
+    # Removing /fs/project from list per asana https://app.asana.com/0/1135148780858012/1203345685227840/f
+    # paths.concat projects.map { |p| Pathname.new("/fs/project/#{p}")  }
 
     # add scratch space directories
     paths << Pathname.new("/fs/scratch/#{User.new.name}")
     paths.concat projects.map { |p| Pathname.new("/fs/scratch/#{p}")  }
 
     # add ess scratch and project directories
-    paths.concat projects.map { |p| Pathname.new("/fs/ess/scratch/#{p}")  }
+    # Removing /fs/ess/scratch from list per asana https://app.asana.com/0/1135148780858012/1203345685227840/f
+    # paths.concat projects.map { |p| Pathname.new("/fs/ess/scratch/#{p}")  }
+
     paths.concat projects.map { |p| Pathname.new("/fs/ess/#{p}")  }
   end
 end

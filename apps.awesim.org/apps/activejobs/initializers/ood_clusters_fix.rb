e9f0003b0f911827945b1e3a341413dd795b3625
Jeff Ohrstrom <johrstrom@osc.edu>
2024-09-18 11:40:12 -0400

    upgrade awesim

--- apps.awesim.org/apps/activejobs/initializers/ood_clusters_fix.rb
+++ apps.awesim.org/apps/activejobs/initializers/ood_clusters_fix.rb
@@ -1,7 +1,9 @@
-OodAppkit.clusters = OodCore::Clusters.new(
-  OodAppkit.clusters.reject { |cluster| [:quick_pitzer].include?(cluster.id) }
-)
+Rails.application.config.after_initialize do
+  OodAppkit.clusters = OodCore::Clusters.new(
+    OodAppkit.clusters.reject { |cluster| [:quick_pitzer].include?(cluster.id) }
+  )
 
-if defined?(OODClusters)
-  OODClusters = OodCore::Clusters.new(OodAppkit.clusters.select(&:job_allow?))
+  if defined?(OODClusters)
+    OODClusters = OodCore::Clusters.new(OodAppkit.clusters.select(&:job_allow?))
+  end
 end

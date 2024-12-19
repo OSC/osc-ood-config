e901a8e6456577e7fbe7610555e5f0f23881a601
Jeff Ohrstrom <johrstrom@osc.edu>
2024-12-19 16:52:02 -0500

    4.0 upgrade (#260)
    Update to 4.0
    
    * update ondemand's initializers for 4.0 to use Rails.application.config.after_initialize blocks.
    * remove NavConfig class.
    * Unrelated to 4.0 - this also removes a few patches that are no longer needed.

--- apps.totalsim.us/apps/dashboard/initializers/ood.rb
+++ apps.totalsim.us/apps/dashboard/initializers/ood.rb
@@ -1,7 +1,6 @@
-# hide all menu options
-NavConfig.categories = ['Files']
-NavConfig.categories_whitelist=true
+Rails.application.config.after_initialize do
 
-# enable dev mode for all configured app developers
-Configuration.app_development_enabled = UsrRouter.base_path.directory?
-Configuration.app_sharing_facls_enabled = true
+  # enable dev mode for all configured app developers
+  Configuration.app_development_enabled = UsrRouter.base_path.directory?
+  Configuration.app_sharing_facls_enabled = true
+end
--- class.osc.edu/apps/dashboard/initializers/stats2480.rb
+++ class.osc.edu/apps/dashboard/initializers/stats2480.rb
@@ -1,18 +1,20 @@
-def add_paths
-  OodFilesApp.candidate_favorite_paths.tap do |paths|
-    # add project space directories
-    projects = User.new.groups.map(&:name).grep(/^P./)
-    paths.concat projects.map { |p| Pathname.new("/fs/project/#{p}")  }
+Rails.application.config.after_initialize do
+  def add_paths
+    OodFilesApp.candidate_favorite_paths.tap do |paths|
+      # add project space directories
+      projects = User.new.groups.map(&:name).grep(/^P./)
+      paths.concat projects.map { |p| Pathname.new("/fs/project/#{p}")  }
 
-    # add scratch space directories
-    paths << Pathname.new("/fs/scratch/#{User.new.name}")
-    paths.concat projects.map { |p| Pathname.new("/fs/scratch/#{p}")  }
+      # add scratch space directories
+      paths << Pathname.new("/fs/scratch/#{User.new.name}")
+      paths.concat projects.map { |p| Pathname.new("/fs/scratch/#{p}")  }
 
-    # add ess scratch and project directories
-    paths.concat projects.map { |p| Pathname.new("/fs/ess/scratch/#{p}")  }
-    paths.concat projects.map { |p| Pathname.new("/fs/ess/#{p}")  }
+      # add ess scratch and project directories
+      paths.concat projects.map { |p| Pathname.new("/fs/ess/scratch/#{p}")  }
+      paths.concat projects.map { |p| Pathname.new("/fs/ess/#{p}")  }
+    end
   end
-end
 
-fs_outage = `grep node_file_test_failure /var/lib/node_exporter/textfile_collector/autofs-file-test.prom | grep -q ' 1'; echo $?`
-add_paths if fs_outage.chomp == "1" && !File.exist?('/etc/ood/config/gpfs_outage')
+  fs_outage = `grep node_file_test_failure /var/lib/node_exporter/textfile_collector/autofs-file-test.prom | grep -q ' 1'; echo $?`
+  add_paths if fs_outage.chomp == "1" && !File.exist?('/etc/ood/config/gpfs_outage')
+end

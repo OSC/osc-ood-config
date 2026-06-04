83bc8f90bbec7bf27e97dcc9acaf44e9eda8bd6c
Zhi-Qiang You <zyou@osc.edu>
2026-06-04 12:25:58 -0400

    Copy ondemand.d files to other portals and add file check

---
+++ test/ondemand_d_test.rb
@@ -0,0 +1,42 @@
+require "test_helper"
+
+class OndemandDTest < Minitest::Test
+  def setup
+    @ondemand_d_dirs = [
+      ONDEMAND.join("ondemand.d"),
+      AWESIM.join("ondemand.d"),
+      CLASS.join("ondemand.d"),
+      TOTALSIM.join("ondemand.d")
+    ]
+  end
+
+  def test_that_ondemand_d_files_are_same_across_portals
+    all_files = @ondemand_d_dirs.map do |dir|
+      Dir.glob(dir.join("**", "*")).map { |f| Pathname.new(f) }.select(&:file?).sort
+    end
+
+    # Check for missing files by relative path
+    rel_files = @ondemand_d_dirs.zip(all_files).map do |dir, files|
+      files.map { |f| f.relative_path_from(dir) }
+    end
+
+    unique_rel_files = rel_files.flatten.uniq
+    missing = []
+    unique_rel_files.each do |rel_file|
+      @ondemand_d_dirs.each do |dir|
+        unless dir.join(rel_file).file?
+          missing << dir.join(rel_file)
+        end
+      end
+    end
+
+    assert missing.empty?, "Missing ondemand.d files:\n  #{missing.join("\n  ")}"
+
+    # Compare file contents
+    all_files.combination(2) do |a_files, b_files|
+      a_files.zip(b_files) do |a, b|
+        assert FileUtils.cmp(a, b), "ondemand.d files differ:\n  #{a}\n  #{b}"
+      end
+    end
+  end
+end

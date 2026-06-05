638ec5f725906034746c790e7d884e94adf7a98d
Zhi-Qiang You <zyou@osc.edu>
2026-06-05 12:50:19 -0400

    Move ondemand.d configs from puppet-conf (#292)
    * Move ondemand.d configs from puppet-conf
    
    * Remove headers
    
    * Copy ondemand.d files to other portals and add file check
    
    * Fix totalsim nav_bar and update the check

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,6 @@ require "pathname"
 ONDEMAND=Pathname.new("ondemand.osc.edu")
 AWESIM=Pathname.new("apps.awesim.org")
 CLASS=Pathname.new("class.osc.edu")
+TOTALSIM=Pathname.new("apps.totalsim.us")
 
 require "minitest/autorun"
